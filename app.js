cbc2d155f3eb3a7823e1d49f7ebafe332c3dcf35
achrefmechergui <achreffcb123achref@gamil.com>
2025-01-19 22:32:03 +0100

    done

---
+++ app.js
@@ -0,0 +1,58 @@
+////// array (data structure) : {} ==> curly braces (alt gr + 4)   [] ===> curly brackets (alt gr + 5) :
+/// methods : (.length (toul) ; )
+//// array are 0 based ;
+
+// const freind1 = "khouloud" ;
+// const friend2 = "nader" ;
+// const freind3 = "maha" ;
+
+// const freinds = ["khouloud", "nader", "maha","houssem","jalila","donia","mohsen","mohamed"];
+//    0         1        2
+//// khouloud index mte3ou 0
+
+// console.log(freinds[index])
+// console.log(freinds[1])
+// console.log(freinds.length) ;
+
+// console.log(freinds[freinds.length-1])
+// console.log(freinds[freinds.length-1]) ///// tal3elna lekhreni ...
+
+//// to add new data ==> push (tzid men teli) ,unshift (tzid men kodem) :
+// const freinds = ["khouloud", "nader", "maha","houssem","jalila","donia"];
+
+// console.log("9bal el push :" , freinds)
+// freinds.push("mohamed")
+// freinds.push("jhon")
+// freinds.unshift("nicole")
+// console.log(freinds)
+
+////// to delete data ===> pop (tnakess men teli) , shift (tnakis men kodem) :
+// const freinds = ["khouloud", "nader", "maha","houssem","jalila","donia"];
+// console.log(freinds)
+// freinds.pop()
+// freinds.pop()
+// console.log(freinds)
+// freinds.shift()
+// console.log(freinds)
+
+////// includes ... ===> traj3ek boolean .... (true or false) ==> mawjoud wela le :
+
+// const x = [200, 100, 500, 250];
+
+
+// console.log(x.includes(10))
+
+////// indexOf ... ===> tlawij al index....
+// const y = ["jhon" ,"nicole" ,"michael"] ; 
+
+// console.log(y.indexOf("michael"))
+
+
+////// how to make edit (modification) in array ... 
+// const country = ["tunisia" , "frensh" , "germany","algeria","marroco","italia","swiss" ,"denmark" ,"sweden"] ;
+// const index = country.indexOf("denmark")
+
+// console.log(country) 
+// country[index] = "belgique"
+// console.log(country)
+
